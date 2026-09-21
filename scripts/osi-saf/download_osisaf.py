52bcc7484b5c871edf5599b00dbb7121914c98f8
chrislisbon <yulashka.htm@yandex.ru>
2026-09-21 14:41:20 +0300

    add load sources

--- scripts/osi-saf/download_osisaf.py
+++ scripts/osi-saf/download_osisaf.py
@@ -4,13 +4,16 @@ from pathlib import Path
 import pandas as pd
 
 
-def name_format(time:str, archive=True):
-    if archive:
+def name_format(time:str, type:str):
+    if type == 'archive':
         return f'ice_conc_nh_ease2-250_cdr-v3p0_{time}1200.nc'
-    else:
-        return None
+    if type == 'deprec':
+        return f'ice_conc_nh_ease2-250_icdr-v3p0_{time}1200.nc'
+    if type == 'current':
+        return f'ice_conc_nh_ease2-250_icdr-v3p0-amsr_{time}1200.nc'
 
-def download(folder_to_save, file_name, ftp,  silent=False):
+
+def download(folder_to_save, file_name, ftp):
     if not os.path.exists(folder_to_save):
         os.makedirs(folder_to_save)
     file = file_name
@@ -24,39 +27,46 @@ def download(folder_to_save, file_name, ftp,  silent=False):
         else:
             print(f'{file} already downloaded')
     except Exception as e:
-        if not silent:
-            print(e)
+        print(e)
         os.remove(Path(folder_to_save, file_to_save))
         return False
     return True
 
 
 def download_pack(folder_to_save, start_day, end_day):
-    archive = True
+    type = 'current' # archive deprec
     dates = pd.date_range(start_day, end_day, freq='1D')
-    if archive:
+    if type == 'archive':
         dir = '/reprocessed/ice/conc/v3p0'
-    else:
-        dir = 'reprocessed/ice/conc-cont-reproc/v3p0'
+    if type == 'deprec':
+        dir = '/reprocessed/ice/conc-cont-reproc/v3p0'
+    if type == 'current':
+        dir = '/reprocessed/ice/conc-cont-reproc-amsr/v3p0'
 
 
     for time in dates:
         month = time.strftime('%m')
         year = time.strftime('%Y')
         time = time.strftime('%Y%m%d')
-        file = name_format(time, archive)
+        file = name_format(time, type)
+        print(file)
         try:
             if not os.path.exists(Path(folder_to_save, file)):
                 ftp = ftplib.FTP('osisaf.met.no')
                 ftp.login()
                 ftp.cwd(f'{dir}/{year}/{month}')
-                print(time)
-                is_downloaded = download(folder_to_save, time, ftp, silent=True)
+                print(f'{dir}/{year}/{month}')
+                is_downloaded = download(folder_to_save, file, ftp)
                 ftp.quit()
         except Exception as e:
             print(e)
             pass
 
 
-folder_to_save = 'D:/Aiice'
-download_pack(folder_to_save, '19790101', '20201231')
+if __name__ == '__main__':
+    # OSI SAF splits the record into three products, set `type` in download_pack:
+    #   'archive' - OSI-450-a, 1979 to 2020
+    #   'deprec'  - OSI-430-a, 2021 to October 2025
+    #   'current' - OSI-438 (AMSR2), from October 2025 onwards
+    folder_to_save = 'path/to/raw/netcdf'
+    download_pack(folder_to_save, '20250701', '20260501')
